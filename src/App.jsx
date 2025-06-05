d263c24babbfd67517c3456bcd8bbac2d25f4601
Echae0 <edujcm01@gmail.com>
2025-06-05 09:47:24 +0900

    웨이팅 대기 페이지 합치기

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainDisplay from './pages/MainDisplay';
 import RestaurantDetail from './pages/RestaurantDetail';
-import WaitingFormPage from './pages/WaitingFormPage'; // ✅ 추가
+import WaitingFormPage from './pages/WaitingFormPage'; 
+import WaitingPage from './pages/WaitingPage';
 import MyPage from './pages/MyPage'
 import LoginPage from './pages/LoginPage'
 import ReviewFormPage from './pages/ReviewFormPage';
@@ -16,8 +17,9 @@ export default function App() {
         <Route path="/" element={<LoginPage />} />  
         <Route path="/maindisplay" element={<MainDisplay />} />
         <Route path="/restaurant/:restaurantId" element={<RestaurantDetail />} />
-        <Route path="/restaurant/:restaurantId/waiting" element={<WaitingFormPage />} /> {/* ✅ 추가 */}
+        <Route path="/restaurant/:restaurantId/waitingform" element={<WaitingFormPage />} /> {/* ✅ 추가 */}
         <Route path="/mypage" element={<MyPage />} /> {/* ✅ 마이페이지 추가 */}
+        <Route path="/restaurant/:restaurantId/waiting" element={<WaitingPage />} />
         <Route path="/reviewformpage" element={<ReviewFormPage />} />
         <Route path="/signUp" element={<SignUpPage />} />
       </Routes>
