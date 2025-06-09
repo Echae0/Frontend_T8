ea90d4e24bb1e5a508ffc682842f1cae66a510e8
lamin5 <say8913@gmail.com>
2025-06-09 10:03:31 +0900

    css 수정

--- src/components/Restaurant_detail/MenuPreview.jsx
+++ src/components/Restaurant_detail/MenuPreview.jsx
@@ -1,14 +1,20 @@
-import styles from './MenuPreview.module.css';
-import PropTypes from 'prop-types';
+import styles from "./MenuPreview.module.css";
+import PropTypes from "prop-types";
 
 const MenuPreview = ({ menu }) => {
   return (
-      <div className={styles.menuPreview}>      
-        <img src={`http://localhost:8080${menu.imageUrl}`} alt={menu.name} className={styles.menuMainImg_pv} />
+    <div className="parentContainer">
+      <div className={styles.menuPreview}>
+        <img
+          src={`http://localhost:8080${menu.imageUrl}`}
+          alt={menu.name}
+          className={styles.menuMainImg_pv}
+        />
         <p className={styles.menuCaption_pv}>{menu.name}</p>
         <p className={styles.menuPrice_pv}>{menu.price?.toLocaleString()}원</p>
         <p className={styles.menuDescription_pv}> {menu.description}</p>
       </div>
+    </div>
   );
 };
 
@@ -18,9 +24,9 @@ MenuPreview.propTypes = {
   menu: PropTypes.shape({
     id: PropTypes.number.isRequired,
     name: PropTypes.string.isRequired,
-    price: PropTypes.number.isRequired, 
+    price: PropTypes.number.isRequired,
     img: PropTypes.string.isRequired,
     imageUrl: PropTypes.string.isRequired,
-    description: PropTypes.string
-  })
+    description: PropTypes.string,
+  }),
 };
